e2a581679d3609753f02688deb44d0793a193d08
unknown <BT_3W2_31@BKTECH.LOCAL>
2023-05-18 12:27:02 -0400

    Initial commit

---
+++ src/Flashcards.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Flashcards {
+    private JTextField word;
+    private JTextArea def;
+    private JButton addCardButton;
+
+    public Flashcards() {
+        word.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+    }
+}
