9e52166b561f1af582df374bc0890e0a62247c6a
BT_3W2_31 <djwqj@g.a>
2023-05-18 12:36:05 -0400

    new

---
+++ src/Card.java
@@ -0,0 +1,17 @@
+public class Card {
+    private String word;
+    private String definition;
+
+    public Card(String w, String d) {
+        word = w;
+        definition = d;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+}
--- src/Flashcards.java
+++ src/Flashcards.java
@@ -1,18 +1,34 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public class Flashcards {
+public class Flashcards extends JFrame implements ActionListener {
+    private ArrayList<Card> cards = new ArrayList<Card>();
     private JTextField word;
     private JTextArea def;
     private JButton addCardButton;
 
     public Flashcards() {
-        word.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                
+        addCardButton.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        if (source instanceof JButton) {
+            JButton button = (JButton) source;
+
+            try {
+                if (button.equals(addCardButton)) {
+                    if (cards.stream().map(card -> card.getWord().equals(word)).count() == 0) {
+                        //add
+                    }
+                }
+            } catch (Exception er) {
+                System.out.println("error because input is bad");
+                throw er;
             }
-        });
+        }
     }
 }
