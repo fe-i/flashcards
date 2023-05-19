9185ce898372d32f56bbd28c7860d0c9b22a2f9a
fe-i <feic47@gmail.com>
2023-05-19 18:23:19 -0400

    done!

--- src/Flashcards.java
+++ src/Flashcards.java
@@ -1,11 +1,12 @@
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Flashcards extends JFrame implements ActionListener {
     private JTabbedPane mainPane;
@@ -20,6 +21,9 @@ public class Flashcards extends JFrame implements ActionListener {
     private JButton next;
     private JPanel currentCard;
     private JButton uploadFileButton;
+    private JPanel quizPanel;
+    private JPanel quizSection;
+    private JButton newQuestionButton;
 
     public Flashcards() {
         setContentPane(mainPane);
@@ -31,6 +35,7 @@ public class Flashcards extends JFrame implements ActionListener {
         prev.addActionListener(this);
         next.addActionListener(this);
         uploadFileButton.addActionListener(this);
+        newQuestionButton.addActionListener(this);
         setVisible(true);
     }
 
@@ -53,6 +58,8 @@ public class Flashcards extends JFrame implements ActionListener {
                         index = cards.size() - 1;
                         updateView();
                     }
+                    word.setText("");
+                    def.setText("");
                 } else if (button.equals(prev)) {
                     if (cards.size() == 0) return;
                     index = index - 1 < 0 ? cards.size() - 1 : index - 1;
@@ -62,14 +69,37 @@ public class Flashcards extends JFrame implements ActionListener {
                     index = index + 1 > cards.size() - 1 ? 0 : index + 1;
                     updateView();
                 } else if (button.equals(uploadFileButton)) {
+                    JOptionPane.showMessageDialog(this, "Make sure the csv file has the format [word,definition].", "Information", JOptionPane.INFORMATION_MESSAGE);
+
                     JFileChooser chooser = new JFileChooser();
+                    chooser.setFileFilter(new FileNameExtensionFilter("CSV Files", "csv"));
                     int returnVal = chooser.showOpenDialog(mainPane);
                     if (returnVal == JFileChooser.APPROVE_OPTION) {
                         File selectedFile = chooser.getSelectedFile();
-                        System.out.println("You chose to copy this file: " +
-                                selectedFile.getName());
-
+                        int count = 0;
+                        try (Scanner scanner = new Scanner(selectedFile)) {
+                            while (scanner.hasNextLine()) {
+                                String line = scanner.nextLine();
+                                String[] data = line.split(",");
+                                if (!contains(data[0])) {
+                                    cards.add(new Card(data[0], data[1]));
+                                    count++;
+                                }
+                            }
+                        } catch (FileNotFoundException er) {
+                            er.printStackTrace();
+                            JOptionPane.showMessageDialog(this, "An error occurred.", "Error", JOptionPane.ERROR_MESSAGE);
+                        }
+                        index = cards.size() - 1;
+                        updateView();
+                        JOptionPane.showMessageDialog(this, "Added " + count + " new words!", "Success", JOptionPane.INFORMATION_MESSAGE);
                     }
+                } else if (button.equals(newQuestionButton)) {
+                    if (cards.size() < 4) {
+                        JOptionPane.showMessageDialog(this, "You need 4 or more words!", "Error", JOptionPane.INFORMATION_MESSAGE);
+                        return;
+                    }
+                    updateQuiz();
                 }
             } catch (Exception er) {
                 JOptionPane.showMessageDialog(this, "An error occurred.", "Error", JOptionPane.ERROR_MESSAGE);
@@ -90,6 +120,8 @@ public class Flashcards extends JFrame implements ActionListener {
             defLabel.setFont(new Font("Times New Roman", Font.PLAIN, 20));
 
             JButton delete = new JButton("Delete Word From List");
+            delete.setFont(prev.getFont());
+            delete.setBackground(Color.RED);
             delete.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
@@ -100,7 +132,7 @@ public class Flashcards extends JFrame implements ActionListener {
             });
 
             currentCard.setLayout(new BoxLayout(currentCard, BoxLayout.Y_AXIS));
-            currentCard.setBackground(Color.ORANGE);
+            currentCard.setBackground(Color.LIGHT_GRAY);
             currentCard.add(wordLabel);
             currentCard.add(new JLabel("________________________________________________________________________________________"));
             currentCard.add(new JLabel("\n"));
@@ -114,6 +146,76 @@ public class Flashcards extends JFrame implements ActionListener {
         if (cards.isEmpty()) {
             JOptionPane.showMessageDialog(this, "Nothing in list! Add something new in the Add Card tab.", "Information", JOptionPane.INFORMATION_MESSAGE);
         }
+    }
+
+    private void updateQuiz() {
+        quizSection.removeAll();
+
+        ArrayList<Card> randomCards = new ArrayList<>();
+        ArrayList<Card> temp = new ArrayList<>(cards);
+        for (int i = 0; i < 4; i++) {
+            int randomIndex = (int) (Math.random() * temp.size());
+            Card randomCard = temp.get(randomIndex);
+            randomCards.add(randomCard);
+            temp.remove(randomIndex);
+        }
+
+        Card card = randomCards.get(0);
+        JLabel defWordLabel = new JLabel("Definition:");
+        defWordLabel.setFont(new Font("Times New Roman", Font.BOLD, 24));
+
+        JLabel def = new JLabel(card.getDefinition());
+        def.setFont(new Font("Times New Roman", Font.BOLD, 18));
+
+        java.util.Collections.shuffle(randomCards);
+
+        JRadioButton[] options = new JRadioButton[4];
+        ButtonGroup buttonGroup = new ButtonGroup();
+        for (int i = 0; i < 4; i++) {
+            options[i] = new JRadioButton(randomCards.get(i).getWord());
+            options[i].setFont(new Font("Times New Roman", Font.PLAIN, 16));
+            buttonGroup.add(options[i]);
+        }
+
+        JButton submitButton = new JButton("Submit");
+        submitButton.setFont(prev.getFont());
+        submitButton.setBackground(Color.YELLOW);
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for (int i = 0; i < 4; i++) {
+                    if (options[i].isSelected()) {
+                        if (randomCards.get(i).equals(card)) {
+                            options[i].setBackground(Color.GREEN);
+                            for (int j = 0; j < 4; j++) {
+                                options[j].setEnabled(false);
+                            }
+                            submitButton.setEnabled(false);
+                            JOptionPane.showMessageDialog(Flashcards.this, "Correct answer!", "Result", JOptionPane.INFORMATION_MESSAGE);
+                        } else {
+                            options[i].setEnabled(false);
+                            options[i].setBackground(Color.RED);
+                            JOptionPane.showMessageDialog(Flashcards.this, "Wrong answer!", "Result", JOptionPane.INFORMATION_MESSAGE);
+                        }
+                        break;
+                    }
+                }
+            }
+        });
+
+        quizSection.setLayout(new BoxLayout(quizSection, BoxLayout.Y_AXIS));
+        quizSection.setBackground(Color.LIGHT_GRAY);
+        quizSection.add(defWordLabel);
+        quizSection.add(def);
+        quizSection.add(new JLabel("________________________________________________________________________________________"));
+        quizSection.add(new JLabel("\n"));
+        for (int i = 0; i < 4; i++) {
+            quizSection.add(options[i]);
+        }
+        quizSection.add(submitButton);
+
+        revalidate();
+        repaint();
     }
 
     private boolean contains(String word) {
